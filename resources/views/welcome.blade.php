192620131dd35b017bec1a11bbe9302ad3190542
Jonas1281989 <jonas.zimmermann@stud.uni-goettingen.de>
2019-08-31 23:55:36 +0200

    myjs,slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                     @endauth
                 </div>
             @endif
-            --!>
+            -->
 
             <div class="content">
                 <div class="title m-b-md">
