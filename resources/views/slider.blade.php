5cd3c72f1725b588cc9c8972d4be84b926a9f46c
malte <maltsomm@gmail.com>
2019-09-11 15:35:25 +0200

    format

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -3,85 +3,85 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/mycss.css" media="screen" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/mycss.css" media="screen"/>
     @include ('footer')
     <title>Slider</title>
-
-    <!-- Fonts -->
-
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-    </style>
 </head>
-    <body>
-     <!--Link von Home-Button noch anpassen-->
-    <div class="left title"><a href="\" style="width: 88px" type="button" class="btn btn-primary" role="button">Home</a></div>
-     <div class="flex-center randgelb">
-        <div class="content randgrün" style="width: 80%">
 
-            <div class="subtitle">
+<body>
+<div class="left title">
+    <a href="\" style="width: 88px" type="button" class="btn btn-primary" role="button">Home</a>
+</div>
+<div class="flex-center">
+    <div class="content randrot" style="width: 80%">
+        <div class="subtitle">
+            <p>Aufgabe: Ziehen Sie alle Slider auf den Wert 50!</p>
+            <p>Anzahl Durchläufe: <span id="durchlauf" style="color:blue ">0</span></p>
+            <p>Aktueller Wert: <span id="sliderWert" style="color:blue ">0</span></p>
+        </div>
 
-                <p>Aufgabe: Ziehen Sie alle Slider auf den Wert 50!</p>
-                <p>Anzahl Durchläufe: <span id="durchlauf" style="color:blue ">0</span></p>
-                <p>Aktueller Wert: <span id="sliderWert" style="color:blue ">0</span></p>
+        <div class="custom-range">
+            <input type="range" min="0" max="40" value="0" step="1" class="custom-range" id="slider1">
+            <script>
+                valueF(0);
+            </script>
+        </div>
 
-            </div>
-            <div class="custom-range randblau">
-                <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider1">
-                <script>
-                    valueF(0);
-                </script>
-            </div>
-            <div class="custom-range randblau">
-                <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider2">
-                <script>
-                    valueF(1);
-                </script>
-            </div>
-            <div class="custom-range randblau">
-                <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider3">
-                <script>
-                    valueF(2);
-                </script>
-            </div>
-            <div class="custom-range randblau">
-                <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider4">
-                <script>
-                    valueF(3);
-                </script>
-            </div>
-            <div class="custom-range randblau">
+        <div class="custom-range ">
+            <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider2">
+            <script>
+                valueF(1);
+            </script>
+        </div>
 
-                <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider5">
-                <script>
-                    valueF(4);
-                </script>
-            </div>
+        <div class="custom-range ">
+            <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider3">
+            <script>
+                valueF(2);
+            </script>
+        </div>
 
-            <div class="randrot" align="right">
+        <div class="custom-range ">
+            <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider4">
+            <script>
+                valueF(3);
+            </script>
+        </div>
 
-                <button style="width: 88px" type="button" class="btn btn-primary" id="ende" onclick="overlayvis()" disabled>Beenden</button>
+        <div class="custom-range ">
+            <input type="range" min="0" max="100" value="0" step="1" class="custom-range" id="slider5">
+            <script>
+                valueF(4);
+            </script>
+        </div>
 
-               <!-- <a href="http://www.google.com"   style="width: 88px" type="button" class="btn btn-primary" role="button">Beenden</a> -->
-                <button style="width: 88px" type="button" class="btn btn-primary" id="weiter" onclick="weiterF()" disabled>Weiter</button>
-            </div>
-            <button  type="button" class="btn btn-primary" id="setFifty" onclick="setFifty()" >setFifty</button>
-            <div class="overlay" id="overlay" style="display: none">
-                <div class="popup" id="popup" style="display: none">
-                    <p>Sie haben den SliderTask <span id="anzdurch" style="color:blue ">0</span> Mal absolviert!</br>Wollen Sie die Anwendung wirklich beenden und </br>zur Umfrage weitergeleitet werden ?</p>
-                    <div class="text-right">
-                        <div style="text-align: center;">
-                            <a href="http://www.google.com" style="width: 88px" class="btn btn-primary">Umfrage</a>
-                            <button style="width: 88px" onclick="overlayinvis()" class="btn btn-primary">Zurück</button>
-                        </div>
-
+        <div align="right">
+            <button style="width: 88px" type="button" class="btn btn-primary" id="ende" onclick="overlayvis()" disabled>
+                Beenden
+            </button>
+            <button style="width: 88px" type="button" class="btn btn-primary" id="weiter" onclick="weiterF()" disabled>
+                Weiter
+            </button>
+        </div>
+        <button type="button" class="btn btn-primary" id="setFifty" onclick="setFifty()">
+            setFifty
+        </button>
+        <div class="overlay" id="overlay" style="display: none">
+            <div class="popup" id="popup" style="display: none">
+                <p>Sie haben den SliderTask <span id="anzdurch" style="color:blue ">0</span> Mal absolviert!</br>Wollen
+                    Sie die Anwendung wirklich beenden und </br>zur Umfrage weitergeleitet werden ?</p>
+                <div class="text-right">
+                    <div style="text-align: center;">
+                        <a href="http://www.google.com" style="width: 88px" class="btn btn-primary">Umfrage</a>
+                        <button style="width: 88px" onclick="overlayinvis()" class="btn btn-primary">Zurück</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
