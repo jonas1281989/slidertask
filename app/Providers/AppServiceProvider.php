88de6e5a0ae39f9dc94c71c2b046f7426431c922
malte <maltsomm@gmail.com>
2019-09-16 17:15:25 +0200

    ass

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,19 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            $url->forceSchema('https');
+        }
     }
 }
