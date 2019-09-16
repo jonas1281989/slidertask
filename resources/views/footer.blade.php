a0310e564aa9c27c36d992bef815bf5b5e115ad3
malte <maltsomm@gmail.com>
2019-09-16 16:56:49 +0200

    ass

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,8 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/mycss.css" media="screen"/>
-<script type="text/javascript" src="{{ URL::asset('js/myjs.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ URL::secure_asset('js/myjs.js') }}"></script>
+<script type="text/javascript" src="{{ URL::secure_asset('js/app.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
