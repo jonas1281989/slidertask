3f17e576bd5b4fc9ea5d8d04f22a314f4af46f42
malte <maltsomm@gmail.com>
2019-09-22 13:50:21 +0200

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-public function boot()
-{
-
-}
+    public function boot()
+    {
+        URL::forceScheme('https');
+    }
 }
