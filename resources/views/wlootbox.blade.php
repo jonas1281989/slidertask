7284acc1d537fc58be2017bde0a701c0499f88a6
malte <maltsomm@gmail.com>
2019-10-14 13:23:10 +0200

    heroku

--- resources/views/wlootbox.blade.php
+++ resources/views/wlootbox.blade.php
@@ -6,13 +6,17 @@
     </head>
     <body>
         <div class="flex-center">
-            <div class="welcome-c">
-                <div class="ueberschrift" style="margin-top: 15%">Slider-Task
+                <div class="welcome-c">
+                    <div class="ueberschrift" style="margin-top: 15%">Slider-Task
+                    </div>
+                    <div><br><br><br></div>
+                    <div class=" links">
+                        <a href="lootbox">Start</a>
+                    </div>
+                    <div class="containerbox"  style="font-size: 2vh;">
+                        <p>Vielen Dank, dass Sie an diesem Experiment teilnehmen! <br>
+                            Klicken Sie auf Start um mit der Aufgabe zu beginnen. Im Anschluss folgt ein kurzer Fragebogen. <br></p>                    </div>
                 </div>
-                <div><br></div>
-                <div class="containerbox"  style="font-size: 2vh;">
-                    Das Experiment ist beendet! Vielen Dank :)</p>
-                </div>
-            </div>                   </div>
-             </body>
+        </div>
+    </body>
 </html>
--- resources/views/wslider.blade.php
+++ resources/views/wslider.blade.php
@@ -9,9 +9,13 @@
                 <div class="welcome-c">
                     <div class="ueberschrift" style="margin-top: 15%">Slider-Task
                     </div>
-                    <div><br></div>
+                    <div><br><br><br></div>
+                    <div class=" links">
+                        <a href="lootbox">Start</a>
+                    </div>
                     <div class="containerbox"  style="font-size: 2vh;">
-                        Das Experiment ist beendet! Vielen Dank :)</p>
+                        <p>Vielen Dank, dass Sie an diesem Experiment teilnehmen! <br>
+                            Klicken Sie auf Start um mit der Aufgabe zu beginnen. Im Anschluss folgt ein kurzer Fragebogen.</p>
                     </div>
                 </div>
         </div>
